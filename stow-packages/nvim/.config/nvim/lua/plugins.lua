985f36bcc08b60195c6a0034e2d7cbcee8b3afc6
daneorie <588451+daneorie@users.noreply.github.com>
2026-03-10 02:17:04 -0400

    Moving jelly config to plugins since HTTP config was also in plugins

--- stow-packages/nvim/.config/nvim/lua/plugins.lua
+++ stow-packages/nvim/.config/nvim/lua/plugins.lua
@@ -149,6 +149,17 @@ require("lazy").setup({
 				},
 				filetype = "http",
 			}
+			parser_config.jelly = {
+				install_info = {
+					url = "https://github.com/OpenJelly/tree-sitter-jelly", -- local path or git repo
+					files = { "src/parser.c", "src/scanner.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+					-- optional entries:
+					branch = "main", -- default branch in case of git repo if different from master
+					generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+					requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+				},
+				--filetype = "jelly", -- if filetype does not match the parser name
+			}
 		end,
 		dependencies = {
 			{ "nvim-treesitter/nvim-treesitter-textobjects", event = "BufReadPre" },
